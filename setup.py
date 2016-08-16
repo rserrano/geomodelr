6e027727f0b5758d6b5e0ea1f523e150c4af3d45
rserrano <rserrano@eafit.edu.co>
2016-08-16 10:04:12 -0500

    Added initial files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='geomodelr',
+      version='0.1',
+      description='GeoModelR is the open source client of the geological modeler.',
+      url='http://github.com/rserrano/geomodelr',
+      author='Ricardo Serrano',
+      author_email='rserrano@mapalomalia.com',
+      license='AGPL',
+      packages=['geomodelr'],
+      zip_safe=False)
+
