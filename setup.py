364fece3e95cd0ad2dd34a98433e87f36e82d6e9
rserrano <rserrano@eafit.edu.co>
2016-12-02 17:19:06 -0500

    Read the docs is not building it.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ cppextension = Extension("cpp", [ 'geomodelr/cpp/match.cpp', 'geomodelr/cpp/geom
                          extra_compile_args=['-std=c++11'])
 
 setup(name='geomodelr',
-    version='0.1',
+    version='0.1.1',
     description='Geomodelr is the open source query tool for geomodelr.com models.',
     url='http://github.com/rserrano/geomodelr',
     author='Ricardo Serrano',
