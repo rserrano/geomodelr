b1eff22424a1d9c062ea07867fdda2dc06d0c00f
Daniel Giraldo <dgiral32@eafit.edu.co>
2017-09-21 15:11:56 -0500

    Basic mesh

--- geomodelr/modflow.py
+++ geomodelr/modflow.py
@@ -17,14 +17,37 @@
 # The utils file contains scripts that can be used by users to 
 # make calculations of their models.
 
+import numpy as np
+import flopy as fp
 
 def create_modflow_inputs( outname, model, grid_divisions, properties):
-	pass
+	
+	num_units = 8
+	X_inf = model.bbox[0]
+	X_sup = model.bbox[3]
+
+	Y_inf = model.bbox[1]
+	Y_sup = model.bbox[4]
+
+	X_vec = np.linspace(X_inf,X_sup,grid_divisions[0])
+	Y_vec = np.linspace(Y_inf,Y_sup,grid_divisions[1])
+ 
+ 	Z_max = np.zeros((grid_divisions[1],grid_divisions[0]))
+
+	for i in np.arange(grid_divisions[1]):
+		for j in np.arange(grid_divisions[0]):
+			Z_max[i,j] = model.height([X_vec[j],Y_vec[i]])
+
+	return(0)
 
 	# model es un model_from_file de geomodelr
 	# propierties deberia ser diccionario
     
+
+    # import geomodelr
 	# model = geomodelr.model_from_file('./modelo.json')
+
+	
 	# model.closest([1,1,1]) -> unidad geologica a la que eprtenece
 	# model.height([1,1]) -> altura del punto ingresado
 	# model.bbox -> cubo que contienen todo el modelo geologico
